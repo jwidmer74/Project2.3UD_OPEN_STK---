be0d13113548d4ddc9aceb03b32d9a457558e64b
Joshua Widmer <jakron74@hotmail.com>
2019-05-16 23:34:47 -0700

    can at least get to login screen and sign it but dead after that

--- application.py
+++ application.py
@@ -1,9 +1,31 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
-from sqlalchemy import create_engine
-from sqlalchemy.sql import func
+from flask import Flask, render_template, request, redirect,\
+                 jsonify, url_for, flash
+import uuid
+from flask import session as login_session
+
+from oauth2client.client import flow_from_clientsecrets, verify_id_token
+from oauth2client.client import FlowExchangeError
+
+import os
+
+import httplib2
+import json
+from flask import make_response
+import requests
+
+
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
 from database_setup2 import Base, Category, Item
+
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+
+CLIENT_ID = json.loads(open("client_secret.json", "r").read())['web']['client_id']
+
+print(CLIENT_ID)
+
 
 app = Flask(__name__)
 
@@ -161,7 +183,107 @@ def editItem(category_id, menu_id):
 
         return render_template('editItem.html', category_id=category_id, menu_id=menu_id, item=editedItem)
 
-# return 'This page is for editing menu item %s' % menu_id
+@app.route('/login')
+def login():
+  state = uuid.uuid4() 
+  login_session['state'] = unicode(str(state),'utf-8')
+  return render_template('login.html', state=state)
+
+
+@app.route('/logout')
+def logout():
+  print(type(login_session))
+  del login_session["username"]
+  return render_template('logout.html')
+
+
+
+
+@app.route('/gconnect', methods=["POST"])
+def gconnect():
+  print(request.args.get("state") ,"==", login_session["state"])
+  if(str(request.args.get("state")) != str(login_session["state"])):
+    print("Invalid State")
+    response = make_response(json.dumps("Invalid State parameter"),401)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+  code = request.data
+  try:
+    print("code", code)
+    oauth_flow = flow_from_clientsecrets(os.path.join(APP_PATH, "client_secret.json"), scope="")
+    oauth_flow.redirect_uri = 'postmessage'
+    oauth_flow.access_type ='offline'
+    credentials = oauth_flow.step2_exchange(code)
+  except FlowExchangeError as e:
+    print('Authentication has failed: {}'.format(str(e)))
+    response = make_response(json.dumps("Failed to upgrade"),401)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+  access_token = credentials.access_token
+
+  url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+  h = httplib2.Http()
+  result = json.loads(h.request(url, 'GET')[1])
+
+  print(result)
+  # If there was an error in the access token info, abort.
+  if result.get('error') is not None:
+      response = make_response(json.dumps(result.get('error')), 500)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+  # Verify that the access token is used for the intended user.
+  gplus_id = credentials.id_token['sub']
+  if result['user_id'] != gplus_id:
+      response = make_response(
+          json.dumps("Token's user ID doesn't match given user ID."), 401)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+  # Verify that the access token is valid for this app.
+  if result['issued_to'] != CLIENT_ID:
+      response = make_response(
+          json.dumps("Token's client ID does not match app's."), 401)
+      print "Token's client ID does not match app's."
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+  stored_access_token = login_session.get('access_token')
+  stored_gplus_id = login_session.get('gplus_id')
+  if stored_access_token is not None and gplus_id == stored_gplus_id:
+      response = make_response(json.dumps('Current user is already connected.'),
+                                200)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+  # Store the access token in the session for later use.
+  login_session['access_token'] = access_token
+  #login_session['gplus_id'] = gplus_id
+
+  # Get user info
+  userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+  params = {'access_token': credentials.access_token, 'alt': 'json'}
+  answer = requests.get(userinfo_url, params=params)
+
+  data = answer.json()
+
+  login_session['username'] = data['name']
+  login_session['picture'] = data['picture']
+  login_session['email'] = data['email']
+
+  output = ''
+  output += '<h1>Welcome, '
+  output += login_session['username']
+  output += '!</h1>'
+  output += '<img src="'
+  output += login_session['picture']
+  output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+  flash("you are now logged in as %s" % login_session['username'])
+  print "done!"
+  return output
 
 # Delete a menu item
 
@@ -179,5 +301,6 @@ def deleteItem(category_id, menu_id):
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+  app.secret_key = 'super_secret_key'
+  app.debug = True
+  app.run(host = '0.0.0.0', port = 5000)
